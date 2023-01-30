06c5f92a7389305d0bcdc94a096cb93f44ab890e
sschr15 <steven.sschr15@hotmail.com>
2023-01-30 17:28:46 -0600

    im dumb and didn't plugin correctly

--- src/main/java/org/duvetmc/risugami/RisugamiMixinPlugin.java
+++ src/main/java/org/duvetmc/risugami/RisugamiMixinPlugin.java
@@ -21,7 +21,7 @@ public class RisugamiMixinPlugin implements IMixinConfigPlugin {
 
 	@Override
 	public boolean shouldApplyMixin(String targetClassName, String mixinClassName) {
-		return false;
+		return true;
 	}
 
 	@Override
